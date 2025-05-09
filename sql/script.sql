c07fcc13df457feae172eed0cbef86b9feaf4c96
uoles <max.uoles@rambler.ru>
2025-05-09 22:06:49 +0300

    Add report

--- sql/script.sql
+++ sql/script.sql
@@ -1,53 +1,82 @@
 
+select *
+from system.parts
+where table = 'user_activity'
 
-SHOW USERS
+select command, is_done
+from system.mutations
+where table = 'user_activity'
 
-SELECT name,
-	auth_type,
-	auth_params
-FROM system.users 
-WHERE storage = 'local_directory'
 
-SELECT *
-FROM system.roles
+CREATE TABLE user_activity
+(
+    activity_date DateTime,
+    user_id UInt32,
+    activity_type LowCardinality(String)
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(activity_date)
+ORDER BY (activity_date);
 
-SELECT *
-FROM system.role_grants
+select *
+from user_activity
 
-SELECT *
-FROM system.grants
-WHERE user_name = 'jhon'
+INSERT INTO user_activity
+SELECT
+	now() - toIntervalSecond(rand() % (365 * 24 * 60 * 60)) AS activity_date,
+	randUniform(1,1000)::Int AS user_id,
+	arrayElement(['login', 'logout', 'edit', 'delete'], randUniform(1,5)::Int) AS activity_type
+FROM system.numbers
+LIMIT 100;
+
+SELECT arrayElement(['login', 'logout', 'edit', 'delete'], randUniform(1,5)::Int)
+
+
+select count(1) as cnt, formatDateTime(activity_date, '%Y-%m-%d') as date
+from user_activity
+group by date
+having cnt > 1
+order by cnt desc;
+
+
+select *
+from user_activity
+where formatDateTime(activity_date, '%Y-%m-%d') = '2024-06-01'
+
+
+
+alter table user_activity
+update activity_type = 'edit'
+where formatDateTime(activity_date, '%Y-%m-%d') = '2024-06-01'
+ 	and activity_type = 'login'
+
+
+select *
+from system.parts
+where table = 'user_activity' and partition like '202405%'
+
+ALTER TABLE user_activity DROP PARTITION '20240511';
+ALTER TABLE user_activity DROP PARTITION '20240514';
+ALTER TABLE user_activity DROP PARTITION '20240515';
+ALTER TABLE user_activity DROP PARTITION '20240516';
+
+ALTER TABLE user_activity DROP PARTITION '20240517';
+ALTER TABLE user_activity DROP PARTITION '20240525';
+ALTER TABLE user_activity DROP PARTITION '20240527';
+ALTER TABLE user_activity DROP PARTITION '20240529';
+
+
+select *
+from user_activity
+where formatDateTime(activity_date, '%Y-%m') = '2024-05'
 
 ----
 
-CREATE USER test_user IDENTIFIED WITH sha256_password BY 'test_password'
+select *
+from system.parts
+where table = 'user_activity' and partition like '202406%'
 
-DROP USER test_user
-
-----
-
-CREATE USER jhon IDENTIFIED WITH sha256_password BY 'qwerty'
-
-CREATE ROLE devs;
-
-GRANT devs TO jhon;
-
-REVOKE devs FROM jhon;
-
-GRANT SELECT ON my_database.* TO devs;
-
-----
-
-SELECT 
-  grants.user_name,
-  grants.role_name,
-  users.name AS role_member,
-  grants.access_type,
-  grants.database,
-  grants.table
-FROM system.grants 
-LEFT JOIN system.role_grants ON grants.role_name = role_grants.granted_role_name
-LEFT JOIN system.users ON role_grants.user_name = users.name
-WHERE users.name = 'jhon'
-  
-
+select *
+from user_activity
+where formatDateTime(activity_date, '%Y-%m') = '2024-06'
+order by activity_date
