a35e8d77cef239ad5942e60b8d5adfe78d4d04fc
uoles <max.uoles@rambler.ru>
2025-05-12 19:19:29 +0300

    Add report

--- sql/script.sql
+++ sql/script.sql
@@ -1,82 +1,90 @@
 
-select *
-from system.parts
-where table = 'user_activity'
-
-select command, is_done
-from system.mutations
-where table = 'user_activity'
-
-
-CREATE TABLE user_activity
+CREATE TABLE my_database.uk_price_paid
 (
-    activity_date DateTime,
-    user_id UInt32,
-    activity_type LowCardinality(String)
+    price UInt32,
+    date Date,
+    postcode1 LowCardinality(String),
+    postcode2 LowCardinality(String),
+    type Enum8('terraced' = 1, 'semi-detached' = 2, 'detached' = 3, 'flat' = 4, 'other' = 0),
+    is_new UInt8,
+    duration Enum8('freehold' = 1, 'leasehold' = 2, 'unknown' = 0),
+    addr1 String,
+    addr2 String,
+    street LowCardinality(String),
+    locality LowCardinality(String),
+    town LowCardinality(String),
+    district LowCardinality(String),
+    county LowCardinality(String)
 )
 ENGINE = MergeTree
-PARTITION BY toYYYYMMDD(activity_date)
-ORDER BY (activity_date);
+ORDER BY (postcode1, postcode2, addr1, addr2);
 
-select *
-from user_activity
 
-INSERT INTO user_activity
+INSERT INTO my_database.uk_price_paid
 SELECT
-	now() - toIntervalSecond(rand() % (365 * 24 * 60 * 60)) AS activity_date,
-	randUniform(1,1000)::Int AS user_id,
-	arrayElement(['login', 'logout', 'edit', 'delete'], randUniform(1,5)::Int) AS activity_type
-FROM system.numbers
-LIMIT 100;
+    toUInt32(price_string) AS price,
+    parseDateTimeBestEffortUS(time) AS date,
+    splitByChar(' ', postcode)[1] AS postcode1,
+    splitByChar(' ', postcode)[2] AS postcode2,
+    transform(a, ['T', 'S', 'D', 'F', 'O'], ['terraced', 'semi-detached', 'detached', 'flat', 'other']) AS type,
+    b = 'Y' AS is_new,
+    transform(c, ['F', 'L', 'U'], ['freehold', 'leasehold', 'unknown']) AS duration,
+    addr1,
+    addr2,
+    street,
+    locality,
+    town,
+    district,
+    county
+FROM url(
+    'http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv',
+    'CSV',
+    'uuid_string String,
+    price_string String,
+    time String,
+    postcode String,
+    a String,
+    b String,
+    c String,
+    addr1 String,
+    addr2 String,
+    street String,
+    locality String,
+    town String,
+    district String,
+    county String,
+    d String,
+    e String'
+) SETTINGS max_http_get_redirects=10;
 
-SELECT arrayElement(['login', 'logout', 'edit', 'delete'], randUniform(1,5)::Int)
+
+SELECT count(*)
+FROM my_database.uk_price_paid
+
+SELECT *
+FROM my_database.uk_price_paid
+limit 100
+
+-----
 
 
-select count(1) as cnt, formatDateTime(activity_date, '%Y-%m-%d') as date
-from user_activity
-group by date
-having cnt > 1
-order by cnt desc;
-
-
+EXPLAIN indexes = 1
 select *
-from user_activity
-where formatDateTime(activity_date, '%Y-%m-%d') = '2024-06-01'
+from my_database.uk_price_paid
+where addr2 = 'PLOT';
 
-
-
-alter table user_activity
-update activity_type = 'edit'
-where formatDateTime(activity_date, '%Y-%m-%d') = '2024-06-01'
- 	and activity_type = 'login'
-
-
+EXPLAIN indexes = 1
 select *
-from system.parts
-where table = 'user_activity' and partition like '202405%'
+from my_database.uk_price_paid
+where price = 14000;
 
-ALTER TABLE user_activity DROP PARTITION '20240511';
-ALTER TABLE user_activity DROP PARTITION '20240514';
-ALTER TABLE user_activity DROP PARTITION '20240515';
-ALTER TABLE user_activity DROP PARTITION '20240516';
-
-ALTER TABLE user_activity DROP PARTITION '20240517';
-ALTER TABLE user_activity DROP PARTITION '20240525';
-ALTER TABLE user_activity DROP PARTITION '20240527';
-ALTER TABLE user_activity DROP PARTITION '20240529';
+EXPLAIN indexes = 1
+select count(*)
+from my_database.uk_price_paid
+where price = 14000;
 
 
-select *
-from user_activity
-where formatDateTime(activity_date, '%Y-%m') = '2024-05'
 
-----
 
-select *
-from system.parts
-where table = 'user_activity' and partition like '202406%'
 
-select *
-from user_activity
-where formatDateTime(activity_date, '%Y-%m') = '2024-06'
-order by activity_date
+
